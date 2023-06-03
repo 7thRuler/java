5b2d149b30e15141de0a1280a2d7b01b8c6e13b6
Subi suku <97378615+7thRuler@users.noreply.github.com>
2023-06-03 14:45:32 +0530

    Add files via upload

---
+++ Testpack.java
@@ -0,0 +1,10 @@
+import calcpack.*;
+public class Testpack{
+public static void main(String[] args)
+{
+calculator c=new calculator();
+c.add(5,10);
+c.difference(100,50);
+}
+}
+
---
+++ calculator.java
@@ -0,0 +1,9 @@
+package calcpack;
+public class calculator{
+public void add(int a,int b){
+System.out.println("sum="+(a+b));
+}
+public void difference(int a,int b){
+System.out.println("difference="+(a-b));
+}
+}
